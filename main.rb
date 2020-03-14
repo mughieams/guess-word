d1bd93b2e54fec959612a3e31db37cabc4ac4687
mughieams <mughieams24@gmail.com>
2020-03-14 18:12:03 +0700

    feat(point): add plus point for correct answer

--- main.rb
+++ main.rb
@@ -2,13 +2,15 @@
 
 require_relative 'src/word'
 require_relative 'src/question'
+require_relative 'src/point'
 
 words = Word.new('data/noun_words.txt').words
 question_pack = Question.new(words)
+point = Point.new(0)
 
 until question_pack.questions.empty?
   print "Tebak kata : #{question_pack.get_question}\n"
   print 'Jawab : '
   answer = gets
-  print "#{question_pack.submit_answer(answer.chomp)}\n"
+  print "#{question_pack.submit_answer(point, answer.chomp)}\n"
 end

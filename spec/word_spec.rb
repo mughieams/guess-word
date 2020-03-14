3ee5b251225e892b70d68150dadfc1f9dfddf484
mughieams <mughieams24@gmail.com>
2020-03-14 15:17:51 +0700

    test(word): create word data values test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require 'rspec'
+require_relative '../src/word'
 
 describe 'Word' do
   before do
-    # Do nothing
+    @answers = Word.new('data/noun_words.txt')
   end
 
   after do
@@ -14,6 +15,16 @@ describe 'Word' do
   context 'check the presence of files' do
     it 'if file exists' do
       expect(true).to be(File.exist?('data/noun_words.txt'))
+    end
+  end
+
+  context 'check values in the words' do
+    it 'the word already exists' do
+      expect(@answers.words).to include('buku')
+    end
+
+    it 'the word does not exists' do
+      expect(@answers.words).to_not include('book')
     end
   end
 end
